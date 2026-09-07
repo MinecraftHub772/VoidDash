0157fd6d5527939160b780cba1a5d037344ed5e8
MinecraftHub772 <tinymiracle2017@gmail.com>
2026-09-08 00:49:17 +0300

    Add logging for VoidDash mod initialization

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+$on_mod(Loaded) {
+    log::info("VoidDash loaded!");
+    log::info("Target: Geometry Dash 2.2081");
+    log::info("Code assistance: ChatGPT");
+}
